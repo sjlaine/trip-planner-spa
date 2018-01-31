ce7d27bc1a3a926dcaa99c0481996fbc84797d39
Alfonso Millan <alfonso.a.millan@gmail.com>
2018-01-31 15:33:13 -0500

    updated

--- public/bundle.js
+++ public/bundle.js
@@ -548,47 +548,80 @@ const map = new mapboxgl.Map({
 });
 
 var fullstackNYCoords = [-74.009151, 40.705086];
-const marker = buildMarker('Activity', fullstackNYCoords)
-console.log(marker);
+const marker = buildMarker('Activity', fullstackNYCoords);
+// console.log(marker);
 
 marker.addTo(map);
 
 addEventListener('DOMContentLoaded', () => {
   fetch('http://localhost:3000/api', {
-  method: 'GET',
-})
-.then(result => {
-  console.log(result, 'type: ', typeof result);
-  return result.json();
-})
-.then(data => {
-  //hotels
-  data[0].forEach((element) => {
-    const hotelDropdown = document.getElementById('hotel-options');
-    const hotelOption = document.createElement('option');
-    const hotelText = document.createTextNode(element.name);
-    hotelOption.appendChild(hotelText);
-    hotelDropdown.append(hotelOption);
+    method: 'GET',
   })
-  //activities
-  data[1].forEach((element) => {
-    const activityDropdown = document.getElementById('activity-options');
-    const activityOption = document.createElement('option');
-    const activityText = document.createTextNode(element.name);
-    activityOption.appendChild(activityText);
-    activityDropdown.append(activityOption);
-  });
-  //restaurants
-    data[2].forEach((element) => {
-    const restaurantDropdown = document.getElementById('restaurant-options');
-    const restaurantOption = document.createElement('option');
-    const restaurantText = document.createTextNode(element.name);
-    restaurantOption.appendChild(restaurantText);
-    restaurantDropdown.append(restaurantOption);
-  });
-})
-.catch(console.error)}
+    .then(result => {
+      // console.log(result, 'type: ', typeof result);
+      return result.json();
+    })
+    .then(data => {
+      //hotels
+      data[0].forEach((element) => {
+        const hotelDropdown = document.getElementById('hotel-options');
+        const hotelOption = document.createElement('option');
+        const hotelText = document.createTextNode(element.name);
+        hotelOption.appendChild(hotelText);
+        hotelDropdown.append(hotelOption);
+      });
+
+      //activities
+      data[1].forEach((element) => {
+        const activityDropdown = document.getElementById('activity-options');
+        const activityOption = document.createElement('option');
+        const activityText = document.createTextNode(element.name);
+        activityOption.appendChild(activityText);
+        activityDropdown.append(activityOption);
+      });
+
+      //restaurants
+      data[2].forEach((element) => {
+        const restaurantDropdown = document.getElementById('restaurant-options');
+        const restaurantOption = document.createElement('option');
+        const restaurantText = document.createTextNode(element.name);
+        restaurantOption.appendChild(restaurantText);
+        restaurantDropdown.append(restaurantOption);
+      });
+
+    })
+    .then(() => {
+
+    })
+    .catch(console.error)
+}
 );
+
+addEventListener('DOMContentLoaded', () => {
+  const hotelButton = document.getElementById('add-hotel');
+  const restaurantButton = document.getElementById('add-restaurant');
+  const activityButton = document.getElementById('add-activity');
+
+  let hotelSelectArr = [...document.getElementById('hotel-options').childNodes];
+  console.dir(hotelSelectArr);
+
+  for (let i = 0; i < hotelSelectArr.length; i++) {
+    console.log('nodes:', hotelSelectArr[i].innerText);
+  }
+
+  hotelButton.addEventListener('click', () => {
+
+  });
+});
+
+
+// const selectedHotel = console.log('test select', document.getElementById('hotel-options').selectedIndex.value);
+
+// if (document.getElementById('hotel-options').selectedIndex === 0) {
+//   console.log('val', document.getElementById('hotel-options').innerHTML);
+// }
+
+
 
 
 /***/ }),
